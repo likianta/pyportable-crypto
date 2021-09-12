e6fee6a9dd44f834de7c6e91e32e8aabedc33710
likianta <likianta@foxmail.com>
2021-09-12 12:52:31 +0800

    Introduce secrets module (since Python 3.6)

--- pyportable_crypto/keygen.py
+++ pyportable_crypto/keygen.py
@@ -1,3 +1,4 @@
+import secrets
 from uuid import uuid1
 
 
@@ -5,5 +6,5 @@ def generate_random_key() -> str:
     return str(uuid1())
 
 
-# def generate_random_keygen(seed: str, step: int, elements):
-#     pass
+def generate_random_bytes(n=16) -> bytes:
+    return secrets.SystemRandom().randbytes(n)
