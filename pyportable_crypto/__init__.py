5294e3f438bfbc75c8fe83cddac7d85d7efee095
likianta <likianta@foxmail.com>
2022-08-01 00:02:11 +0800

    Change CLI tool to `argsense`

--- pyportable_crypto/__init__.py
+++ pyportable_crypto/__init__.py
@@ -18,4 +18,4 @@ from .decrypt import decrypt_file
 from .encrypt import encrypt_data
 from .encrypt import encrypt_file
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
--- pyportable_crypto/__main__.py
+++ pyportable_crypto/__main__.py
@@ -1,27 +1,26 @@
+import os
+
 try:
-    from fire import Fire
-except ImportError:
-    input('Please install fire first: `pip install fire`')
-    import sys
-    
-    sys.exit(1)
-else:
-    import os
-    from .compiler import Compiler
+    from argsense import cli
+except Exception:
+    print('Command line tool is not installed! Please install it by pip: \n'
+          '\tpip install argsense')
+    exit(-1)
+
+from .compiler import Compiler
 
 
-class CLI:
-    
-    @staticmethod
-    def compile_file(file_i, file_o, key):
-        compiler = Compiler(str(key), os.path.dirname(file_o))
-        compiler.compile_file(file_i, file_o)
-    
-    @staticmethod
-    def compile_dir(dir_i, dir_o, key):
-        compiler = Compiler(str(key), dir_o)
-        compiler.compile_dir(dir_i, dir_o)
+@cli.cmd()
+def compile_file(file_i: str, file_o: str, key: str):
+    compiler = Compiler(key, os.path.dirname(file_o))
+    compiler.compile_file(file_i, file_o)
+
+
+@cli.cmd()
+def compile_dir(dir_i: str, dir_o: str, key: str):
+    compiler = Compiler(key, dir_o)
+    compiler.compile_dir(dir_i, dir_o)
 
 
 if __name__ == '__main__':
-    Fire(CLI())
+    cli.run()
