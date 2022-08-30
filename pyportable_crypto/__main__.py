0fc3af60c09d6e577b8c023d4391736009530e10
likianta <likianta@foxmail.com>
2022-08-31 01:43:12 +0800

    Fix python 3.8 typing compatibility

--- pyportable_crypto/__main__.py
+++ pyportable_crypto/__main__.py
@@ -1,13 +1,30 @@
-import os
-
 try:
-    from argsense import cli
-except Exception:
-    print('Command line tool is not installed! Please install it by pip: \n'
-          '\tpip install argsense')
+    from argsense import cli  # noqa
+except ImportError:
+    print(':v4', 'Command line tool not found!\n'
+                 'Please install it with: `pip install argsense`')
     exit(-1)
 
+import os
+
+from . import __version__
 from .compiler import Compiler
+from .encrypt import encrypt_data
+
+print(__version__)
+
+
+@cli.cmd()
+def encrypt_text(text: str, key: str = None):
+    """
+    kwargs:
+        key: if not given, will generate a random key.
+    """
+    if key is None:
+        from secrets import token_urlsafe
+        key = token_urlsafe()
+    print(key)
+    print('\n' + str(encrypt_data(text, key)), ':s')
 
 
 @cli.cmd()
