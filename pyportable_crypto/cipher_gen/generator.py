7de4517cd7899e3ae773f8e4afa3753db3b190b3
Likianta <likianta@foxmail.com>
2024-05-29 01:56:20 +0800

    No given external directory to cipher_gen

--- pyportable_crypto/cipher_gen/generator.py
+++ pyportable_crypto/cipher_gen/generator.py
@@ -1,11 +1,9 @@
-import re
 import sys
 from hashlib import md5
 from platform import system
 
 from lk_utils import dumps
 from lk_utils import fs
-from lk_utils import loads
 from lk_utils import run_cmd_args
 from lk_utils import xpath
 from lk_utils.textwrap import dedent
@@ -14,94 +12,81 @@ SYSTEM = system().lower()
 
 
 def generate_cipher_package(
-    dir_o: str, key: str, python_executable_path: str = sys.executable
+    key: str, python_executable_path: str = sys.executable
 ) -> str:
     """
     params:
         key: the secret key.
             - can be any string with any length.
             - cannot be empty.
-        dir_o:
-            the dirname must be a valid python package name, for example -
-            "pyportable_runtime".
-            for safety consideration, if the dir_o exists, will stop and raise -
-            FileExistsError.
     """
-    assert not fs.exists(dir_o), dir_o
     assert key, 'key cannot be empty!'
-    assert re.compile(r'[a-zA-Z_]\w*'), \
-        'the dirname should be a valid python package name format!'
-    
-    package_name = fs.dirname(dir_o)
-    print(':v2', package_name, key)
+    # assert re.compile(r'[a-zA-Z_]\w*'), \
+    #     'the dirname should be a valid python package name format!'
     
     dir_i = xpath('.')  # current dir
     dir_m = xpath('_cache/{}'.format(md5(key.encode('utf-8')).hexdigest()))
-    fs.make_dir(dir_o)
+    dir_o = '{}/pyportable_runtime'.format(dir_m)
+    if fs.exists(dir_o):
+        return dir_o
+    else:
+        fs.make_dirs(dir_o)
     
     file_i = '{}/cipher_standalone.py'.format(dir_i)
     file_m = '{}/cipher.py'.format(dir_m)
-    file_o = '{}/cipher.{}'.format(
-        dir_o, 'pyd' if SYSTEM == 'windows' else 'so'
+    file_o = '{}/cipher.{}'.format(dir_o, 'pyd' if SYSTEM == 'windows' else 'so')
+    
+    # -------------------------------------------------------------------------
+    
+    code = fs.load(file_i)
+    assert '__KEY__' in code
+    code = code.replace('__KEY__', key)
+    fs.dump(code, file_m)
+    
+    print('compiling... (this may take several minutes)', ':v3st2')
+    run_cmd_args(
+        python_executable_path,
+        # note we don't use abspath because the path in poetry virtual env -
+        # may be very long, which will cause windows msvc linking crashed.
+        'cythonize.py',  # file in current dir.
+        fs.relpath(file_m, dir_i),
+        'build_ext',
+        '--inplace',
+        cwd=dir_i,
     )
+    print('compilation done', ':t2')
+    fs.move(fs.find_file_paths(dir_m, ('.pyd', '.so'))[0], file_o)
     
-    if fs.exists(dir_m):
-        print('use cache', dir_m)
-    else:
-        fs.make_dir(dir_m)
-        
-        code = loads(file_i)
-        assert '__KEY__' in code
-        code = code.replace('__KEY__', key)
-        dumps(code, file_m)
-        
-        print('compiling... (this may take several minutes)', ':v3st2')
-        run_cmd_args(
-            python_executable_path,
-            # note we don't use abspath because the path in poetry virtual env -
-            # may be very long, which will cause windows msvc linking crashed.
-            'cythonize.py',  # file in current dir.
-            fs.relpath(file_m, dir_i),
-            'build_ext',
-            '--inplace',
-            cwd=dir_i,
-        )
-        print('compilation done', ':t2')
-        
-        from pyportable_crypto import __version__ as crypto_version
-        pyversion = sys.version_info[:2]  # e.g. (3, 8)
-        dumps(dedent('''
-            encrypt = None
-            decrypt = None
+    from pyportable_crypto import __version__ as crypto_version
+    pyversion = sys.version_info[:2]  # e.g. (3, 8)
+    dumps(dedent('''
+        encrypt = None
+        decrypt = None
 
 
-            def _init_check():
-                import sys
+        def _init_check():
+            import sys
 
-                current_pyversion = sys.version_info[:2]  # type: tuple
-                target_pyversion = {0}
-                if current_pyversion != target_pyversion:
-                    raise Exception(
-                        "Python interpreter version doesn't matched!",
-                        "Required: Python {{}}, got {{}} ({{}})".format(
-                            ', '.join(map(str, target_pyversion)),
-                            ', '.join(map(str, current_pyversion)),
-                            sys.executable
-                        )
+            current_pyversion = sys.version_info[:2]  # type: tuple
+            target_pyversion = {0}
+            if current_pyversion != target_pyversion:
+                raise Exception(
+                    "Python interpreter version doesn't matched!",
+                    "Required: Python {{}}, got {{}} ({{}})".format(
+                        ', '.join(map(str, target_pyversion)),
+                        ', '.join(map(str, current_pyversion)),
+                        sys.executable
                     )
+                )
 
 
-            _init_check()
-            del _init_check
+        _init_check()
+        del _init_check
 
-            from .cipher import decrypt  # noqa
-            from .cipher import encrypt  # noqa
+        from .cipher import decrypt  # noqa
+        from .cipher import encrypt  # noqa
 
-            __version__ = '{1}'
-        ''').format(pyversion, crypto_version), dir_o + '/__init__.py')
+        __version__ = '{1}'
+    ''').format(pyversion, crypto_version), f'{dir_o}/__init__.py')
     
-    fs.copy_file(fs.find_file_paths(dir_m, ('.pyd', '.so'))[0], file_o)
-    fs.copy_file('{}/__init__.py'.format(dir_i), '{}/__init__.py'.format(dir_o))
-    
-    print('done. see result at "{}"'.format(dir_o), ':t')
-    return package_name
+    return dir_o
