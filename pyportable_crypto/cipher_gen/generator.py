a297135ee3bf1a3f936aa936559e879021501626
Likianta <likianta@foxmail.com>
2024-06-12 21:09:30 +0800

    Fallback behavior when cipher gen failed

--- pyportable_crypto/cipher_gen/generator.py
+++ pyportable_crypto/cipher_gen/generator.py
@@ -48,16 +48,20 @@ def generate_cipher_package(
     fs.dump(code, file_m)
     
     print('compiling... (this may take several minutes)', ':v3st2')
-    run_cmd_args(
-        python_executable_path,
-        # note we don't use abspath because the path in poetry virtual env -
-        # may be very long, which will cause windows msvc linking crashed.
-        'cythonize.py',  # file in current dir.
-        fs.relpath(file_m, dir_i),
-        'build_ext',
-        '--inplace',
-        cwd=dir_i,
-    )
+    try:
+        run_cmd_args(
+            python_executable_path,
+            # note we don't use abspath because the path in poetry virtual env -
+            # may be very long, which will cause windows msvc linking crashed.
+            'cythonize.py',  # file in current dir.
+            fs.relpath(file_m, dir_i),
+            'build_ext',
+            '--inplace',
+            cwd=dir_i,
+        )
+    except Exception:
+        fs.remove_tree(dir_o)
+        raise
     print('compilation done', ':t2')
     fs.move(fs.find_file_paths(dir_m, ('.pyd', '.so'))[0], file_o)
     
